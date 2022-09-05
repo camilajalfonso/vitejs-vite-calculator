1b17ba095f5835d0ebd722e99b5ecd1ec1cf2805
Jose Alfonso <jose.alfonso@apple.com>
2022-09-05 10:11:58 -0700

    chore: home page links to tutorials

--- main.js
+++ main.js
@@ -10,7 +10,11 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello Cami!</h1>
+    <div class="links">
+      <a href="calculator.html">Calculator</a>
+      <a href="playground.html">Playground</a>
+    </div>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
