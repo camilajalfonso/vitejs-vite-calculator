b9396a0b08188989ead99337eff59bae420b9f99
camilajalfonso <camilaalfonso@me.com>
2022-09-07 17:50:55 -0700

    fix: calculatorEvents

--- calculator.js
+++ calculator.js
@@ -36,13 +36,13 @@ class Calculator{
 
 // console.log('---hello Cami---');
 const numberButtons = document.querySelectorAll('[data-number]')
-console.log (numberButtons)
+// console.log (numberButtons)
 const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelectorAll('[data-equals]')
-const deleteButton = document.querySelectorAll('[data-delete]')
-const allClearButton = document.querySelectorAll('[data-all-clear]')
-const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
-const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
