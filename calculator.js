60f9329de4836ed2a079f8a22aa2032d761ad53a
camilajalfonso <camilaalfonso@me.com>
2022-09-07 17:25:25 -0700

    chore.calculatorUpdate

--- calculator.js
+++ calculator.js
@@ -1,5 +1,59 @@
 import './style.css'
 import './calculator.css'
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
 
-console.log('---hello Cami---');
+    delete() {
 
+    }
+
+    appendNumber(number) {
+        this.currentOperand = number
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+
+}
+
+// console.log('---hello Cami---');
+const numberButtons = document.querySelectorAll('[data-number]')
+console.log (numberButtons)
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelectorAll('[data-equals]')
+const deleteButton = document.querySelectorAll('[data-delete]')
+const allClearButton = document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+// allClearButton.addEventListener('click', button => {
+//     calculator.clear()
+//     calculator.updateDisplay()
+// })
